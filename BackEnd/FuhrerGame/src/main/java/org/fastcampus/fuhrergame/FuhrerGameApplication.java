7afa9129b4fb49d287a2e2869a905f3edc33cbbf
XungHi <tjddnjs612@gmail.com>
2025-01-04 12:06:09 +0900

    BackEnd setting
    1st try

---
+++ BackEnd/FuhrerGame/src/main/java/org/fastcampus/fuhrergame/FuhrerGameApplication.java
@@ -0,0 +1,13 @@
+package org.fastcampus.fuhrergame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuhrerGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuhrerGameApplication.class, args);
+    }
+
+}
---
+++ BackEnd/FuhrerGame/src/test/java/org/fastcampus/fuhrergame/FuhrerGameApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fastcampus.fuhrergame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuhrerGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
